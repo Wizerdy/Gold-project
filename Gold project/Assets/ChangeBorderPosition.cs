adc0f195d9cb1433c86faa983951a8250d9d6800
Laefar <53569867+Laefar@users.noreply.github.com>
2020-05-17 14:17:08 +0200

    Add Border Shader + Assets

---
+++ Gold project/Assets/ChangeBorderPosition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeBorderPosition : MonoBehaviour
+{
+    public Material grayScaleBorder;
+
+    void Update()
+    {
+        grayScaleBorder.SetFloat("_Border", transform.position.x*-1);
+    }
+}
