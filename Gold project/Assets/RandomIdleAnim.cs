b6219f04a7e963bcc53f802e05edde143762a0ac
Laefar <53569867+Laefar@users.noreply.github.com>
2020-05-19 11:39:37 +0200

    Ajout d'animation pour l'herbe

---
+++ Gold project/Assets/RandomIdleAnim.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomIdleAnim : MonoBehaviour
+{
+    private Animator anim;
+    public int herbe = 0;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+        anim.SetInteger("isHerbe", herbe);
+        anim.SetInteger("IdleIndex", Random.Range(0, 2));
+    }
+
+}
