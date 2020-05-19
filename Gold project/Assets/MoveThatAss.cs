8dc1ae703997d8aedc2d4d104e922cf9be6a7bf3
Laefar <53569867+Laefar@users.noreply.github.com>
2020-05-19 15:52:28 +0200

    Ajout de l'animation d'attaque et de "marche"

---
+++ Gold project/Assets/MoveThatAss.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveThatAss : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+    void Update()
+    {
+        transform.position = Vector2.Lerp(transform.position, target.position, speed*Time.deltaTime);
+    }
+}
