9a7c8989972999ed9dce0a3c213d3d78b349a01c
wiredii <grancru2000@gmail.com>
2020-05-29 16:20:39 +0200

    Menu
    Sauve

---
+++ Gold project/Assets/Scripts/GreyScaleMouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreyScaleMouse : MonoBehaviour
+{
+    public Material grayScaleBorder;
+
+    private void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            grayScaleBorder.SetFloat("_Border", Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).x * -1);
+            Debug.Log(Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position).x);
+        }
+
+    }
+}
