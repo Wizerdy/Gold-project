a27d42dd9463e48627007b87a601b17ac12e2756
Laefar <53569867+Laefar@users.noreply.github.com>
2020-06-08 19:39:13 +0200

    L'état veut un une entité neutre. Donc les avant-post fut créée
    NeutralSpawner est à attacher à l'objet de base d'un avant-poste.
    
    Le script possède 5 variable publique :
    nbOfSlime => Le nombre de slime instancié par vague.
    cdBetweenSlime => Coolddown entre chaque instanciation de slime.
    cdBetweenWave => Cooldown entre chaque vague.
    target => La cible que le slime doit attaquer (à laisser vide si l'outpost doit "observer" d'autre avant-poste)
    lookOut => Une liste d'avant-postes à observer. Si des éléments y sont ajouté, alors target doit rester vide.

---
+++ Gold project/Assets/NeutralSpawner.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeutralSpawner : MonoBehaviour
+{
+    public bool towerIsTaken = false;
+    private NeutralSpawner isLinkedTo;
+
+    [Space]
+    public int nbOfSlime = 3;
+    public float cdBetweenSlime = .5f;
+    public float cdBetweenWave = 5;
+    private bool waitingBetweenWave = false;
+
+    [Space]
+    public GameObject target;
+    private Tower towerInstance;
+
+    [Space]
+    public NeutralSpawner[] lookOut;
+    private List<GameObject> isTaken;
+
+    void Start()
+    {
+        towerInstance = GetComponent<Tower>();
+
+        if(lookOut.Length > 0)
+        {
+            foreach(NeutralSpawner lookFor in lookOut)
+            {
+                lookFor.isLinkedTo = GetComponent<NeutralSpawner>();
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(lookOut.Length > 0 && isTaken.Count > 0)
+        {
+            foreach (GameObject tower in isTaken)
+            {
+                StartCoroutine(SpawnNeutralForce(tower.GetComponent<NeutralSpawner>().target));
+            }
+        }
+
+        if(lookOut.Length == 0 && towerInstance.side != Unit.Side.NEUTRAL)
+        {
+
+            StartCoroutine(SpawnNeutralForce(target));
+        }
+
+        //Je veux pas rajouter deux ligne dans le script de Flo, j'ai peur de me faire tapper :'(
+        if(towerInstance.side != Unit.Side.NEUTRAL && towerIsTaken == false)
+        {
+            isLinkedTo.isTaken.Add(gameObject);
+            towerIsTaken = true;
+        }
+    }
+
+    IEnumerator SpawnNeutralForce(GameObject target)
+    {
+        if(waitingBetweenWave == false)
+        {
+            waitingBetweenWave = true;
+
+
+            for(int i = 0; i < nbOfSlime; i++)
+            {
+                GameObject obj = GameManager.instance.InstantiateUnit(GameManager.instance.units["WHITE"], Unit.Side.NEUTRAL, towerInstance.spawnPoint.position);
+                obj.GetComponent<Knight>().target = target.transform;
+                    
+                switch(target.name)
+                {
+                    case "Ally":
+                        obj.transform.rotation = Quaternion.Euler(0, 180, 0);
+                        break;
+                    case "Enemies":
+                        obj.transform.rotation = Quaternion.Euler(0, 0, 0);
+                        break;
+                }
+
+
+                yield return new WaitForSeconds(cdBetweenSlime);
+            }
+
+            yield return new WaitForSeconds(cdBetweenWave);
+            waitingBetweenWave = false;
+        }
+    }
+
+}
