af709e63ea9ec0a1fa5c7afdd70b946e2469ace4
wiredii <grancru2000@gmail.com>
2020-06-10 20:16:28 +0200

    Push
    Push push
    
    Normalement, je règle le problème de stack overflow, et au pire on peut le désactiver dans le menu pause

---
+++ Gold project/Assets/Scripts/DestroyCoroutine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCoroutine : MonoBehaviour
+{
+    [SerializeField] private float lifeTime;
+    [SerializeField] private bool fade = true;
+    [SerializeField] private SpriteRenderer sprRend;
+
+    void Start()
+    {
+        StartCoroutine("Life", lifeTime);
+    }
+
+    private IEnumerator Life(float time)
+    {
+        yield return new WaitForSeconds(time);
+        if (fade)
+            StartCoroutine("fade");
+        else
+            Destroy(gameObject);
+    }
+
+    private IEnumerator Fade()
+    {
+        float timePassed = 0f;
+
+        while(timePassed < 1f)
+        {
+            yield return new WaitForSeconds(0.05f);
+            timePassed += 0.05f;
+            sprRend.color = new Color(sprRend.color.r, sprRend.color.g, sprRend.color.b, Tools.Map(timePassed, 0f, 1f, 0, sprRend.color.a));
+        }
+        Destroy(gameObject);
+    }
+}
